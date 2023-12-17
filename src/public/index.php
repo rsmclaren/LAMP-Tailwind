b4473542723183490f44a6ab18b97634d3870f44
Ryan McLaren <ryansm@outlook.com>
2023-12-16 22:23:21 -0400

    updated stylesheet and main.bundle.js paths to use webpack dev server

--- src/public/index.php
+++ src/public/index.php
@@ -2,17 +2,17 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>PHP</title>
-  <link rel="stylesheet" href="./assets/dist/main.css" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP</title>
+    <link rel="stylesheet" href="http://localhost:3000/assets/main.css" />
 </head>
 
 <body class="m-4">
-  <h1 class="text-2xl">LAMP with Tailwind</h1>
+    <h1 class="text-2xl">LAMP with Tailwind</h1>
 </body>
 
-<script src=" ./assets/dist/main.bundle.js">
+<script src="http://localhost:3000/assets/main.bundle.js">
 </script>
 
 </html>
